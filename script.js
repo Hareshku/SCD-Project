e5848c102c64cf7e9204d5dc6237e04ca8d861a8
Hareshku <hareesh7737@gmail.com>
2024-03-05 17:43:53 +0500

    few code updated in all pages

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function() {
   var navbar = document.querySelector('.header');
   if (window.scrollY > 100) {
-    navbar.style.backgroundColor = 'red'; // Change to desired color
+    navbar.style.backgroundColor = '#175d69'; // Change to desired color
   } else {
     navbar.style.backgroundColor = ''; // Reset to initial color
   }
