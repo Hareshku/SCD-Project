6bc1d8dda92c00612aa6de8fb7f78c60bf531202
Hareshku <hareesh7737@gmail.com>
2024-03-05 20:49:51 +0500

    few unnecessary code deleted

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function() {
   var navbar = document.querySelector('.header');
   if (window.scrollY > 100) {
-    navbar.style.backgroundColor = '#175d69'; // Change to desired color
+    navbar.style.backgroundColor = '#175d69'; // Change color
   } else {
     navbar.style.backgroundColor = ''; // Reset to initial color
   }
